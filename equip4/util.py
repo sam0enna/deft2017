78f1c415810ee1644c30dba3ecff46c6a4639cad
JonathanBonnaud <bonnjona@hotmail.fr>
2017-11-13 19:19:23 +0100

    Delete util.py

--- equip4/util.py
+++
@@ -1,7 +0,0 @@
-def get_emoticones():
-    with open("../ressrc/emoticones.txt") as f:
-        for i in range(8):
-            next(f)
-        content = f.readlines()
-    # to remove whitespace characters like `\n` at the end of each line
-    return [x.strip() for x in content if x.strip()]
